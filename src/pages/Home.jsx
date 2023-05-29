69bbff96815caf3ff301f2f0e209b20da9d298af
Andreyhuey <andreyhuey777@gmail.com>
2023-05-29 03:02:26 +0100

    first commit

--- src/pages/Home.jsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <div class="h-screen bg-zinc-200 dark:bg-slate-900"></div>;
-}
--- src/pages/Todos.jsx
+++
@@ -1,3 +0,0 @@
-export default function Todos() {
-  return <div className="h-screen bg-zinc-200 dark:bg-slate-900"></div>;
-}
